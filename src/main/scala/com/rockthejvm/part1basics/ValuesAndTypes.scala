57dfa96a528372b2bc8dd5628749b5cef78154e6
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-04-18 11:30:58 -0400

    Complete values and types

---
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,29 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  // values
+  val meaningOfLife: Int = 42 // const int meaningOfLife = 42
+
+  // reassignment not allowed
+  // meaningOfLife = 45
+
+  // type inference
+  val anInteger = 67 // : Int is optional
+
+  // common types
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aShort: Short = 5263 // 16 bits
+  val anInt: Int = 2000000000 // 32 bits
+  val aLong: Long = 94738492018493L // 64 bits
+  val aFloat: Float = 2.4f // 32 bits
+  val aDouble: Double = 3.14 // 64 bits
+
+  // string
+  val aString: String = "Scala"
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
