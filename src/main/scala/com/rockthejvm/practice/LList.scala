ce1b3b36ccce4004b579916c3edb2681c68c619f
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-05-05 12:03:33 -0400

    Extend LList with find method

--- src/main/scala/com/rockthejvm/practice/LList.scala
+++ src/main/scala/com/rockthejvm/practice/LList.scala
@@ -128,6 +128,15 @@ class DoublerList extends Transformer[Int, LList[Int]] {
   override def transform(value: Int): LList[Int] = Cons(value, Cons(value + 1, Empty()))
 }
 
+object LList {
+  @tailrec
+  def find[A](list: LList[A], predicate: Predicate[A]): A = {
+    if (list.isEmpty) throw new NoSuchElementException
+    else if (predicate.test(list.head)) list.head
+    else find(list.tail, predicate)
+  }
+}
+
 object LListTest {
   def main(args: Array[String]): Unit = {
     val empty = Empty[Int]()
@@ -169,5 +178,11 @@ object LListTest {
     // flatMap testing
     val flattenedList = first3Numbers.flatMap(new DoublerList)
     println(flattenedList)
+
+    // find test
+    println(LList.find(first3Numbers, evenPredicate))
+    //  println(LList.find(first3Numbers, new Predicate[Int] {
+    //    override def test(element: Int): Boolean = element > 5
+    //  })) // throws a NSEException
   }
 }
