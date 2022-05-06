6b8e7d7826831c3ea077e8b44ae50ebd26d13dab
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-05-06 09:40:33 -0400

    Update preventing inheritance

--- src/main/scala/com/rockthejvm/part2oop/PreventingInheritance.scala
+++ src/main/scala/com/rockthejvm/part2oop/PreventingInheritance.scala
@@ -14,13 +14,13 @@ object PreventingInheritance {
   // class Cat extends Animal // illegal
 
   // sealing a type hierarchy = inheritance only permitted inside this file
-  class Guitar(nStrings: Int)
+  sealed class Guitar(nStrings: Int)
   class ElectricGuitar(nStrings: Int) extends Guitar(nStrings)
   class AcousticGuitar extends Guitar(6)
 
   // no modifier = "not encouraging" inheritance
   // open = specifically marked for extension
-  // not mandatory, good practice
+  // not mandatory, good practice (should be accompanied by documentation on what extension implies)
   open class ExtensibleGuitar(nStrings: Int)
 
   def main(args: Array[String]): Unit = {
