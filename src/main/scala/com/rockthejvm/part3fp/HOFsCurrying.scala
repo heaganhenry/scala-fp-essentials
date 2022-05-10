86e41512f2612a11f6b4b7dab1564a263fb770c0
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-05-10 11:50:24 -0400

    Complete hof exercises

--- src/main/scala/com/rockthejvm/part3fp/HOFsCurrying.scala
+++ src/main/scala/com/rockthejvm/part3fp/HOFsCurrying.scala
@@ -54,6 +54,25 @@ object HOFsCurrying {
   val standardFormat: (Double => String) = curriedFormatter("%4.2f") // (x: Double) => "%4.2f".format(x)
   val preciseFormat: (Double => String) = curriedFormatter("%10.8f") // (x: Double) => "%10.8f".format(x)
 
+/**
+ * 1. LList exercises
+ *    - foreach(A => Unit): Unit
+ *      [1,2,3].foreach(x => println(x))
+ *
+ *    - sort((A, A) => Int): LList[A]
+ *      [3,2,4,1].sort((x, y) => x - y) = [1,2,3,4]
+ *      (hint: use insertion sort)
+ *
+ *    - zipWith[B](LList[A], (A, A) => B): LList[B]
+ *      [1,2,3].zipWith([4,5,6], x * y) => [1 * 4, 2 * 5, 3 * 6] = [4, 10, 18]
+ *
+ *    - foldLeft[B](start: B)((A, B) => B): B
+ *      [1,2,3,4].foldLeft[Int](0)(x + y) = 10
+ *      0 + 1 = 1
+ *      1 + 2 = 3
+ *      3 + 3 = 6
+ *      6 + 4 = 10
+ */
   def main(args: Array[String]): Unit = {
     println(tenThousand_v2)
   }
